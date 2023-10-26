b320f009bf2ab82f33ea0b67ed1dce45d921e9b7
casagrande7 <joaofootz10@gmail.com>
2023-10-26 15:11:06 -0300

    Update

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import CadastroClientes from "../components/CadastroClientes";
 import ListagemClientes from "../components/ListagemClientes";
+import BuscaCep from "../components/BuscaCepFormCliente";
 
 const AppRouter = () => {
     return(
@@ -9,6 +10,7 @@ const AppRouter = () => {
           <Routes>
             <Route path="/CadastroClientes" element= {<CadastroClientes />} />
             <Route path="/ListagemClientes" element= {<ListagemClientes />} />
+            <Route path="/BuscaCep" element= {<BuscaCep />} />
            </Routes>
         </BrowserRouter>
 
